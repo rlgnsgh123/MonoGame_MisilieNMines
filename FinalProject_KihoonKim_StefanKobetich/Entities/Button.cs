c68cb03f669435e9748b79362702f3e089719b16
김기훈 <rlgnsgh123@naver.com>
2023-12-05 13:21:59 -0500

    playMenuScene

---
+++ FinalProject_KihoonKim_StefanKobetich/Entities/Button.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace FinalProject_KihoonKim_StefanKobetich.Entities
+{
+    public class Button
+    {
+        private Vector2 position;
+        private string text;
+        private Color backgroundColor;
+        private Color hoverColor;
+        private SpriteFont font;
+        private GraphicsDevice graphicsDevice;
+
+        public Button(Vector2 position, string text, Color backgroundColor, Color hoverColor, SpriteFont font, GraphicsDevice graphicsDevice)
+        {
+            this.position = position;
+            this.text = text;
+            this.backgroundColor = backgroundColor;
+            this.hoverColor = hoverColor;
+            this.font = font;
+            this.graphicsDevice = graphicsDevice;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            // Draw button background
+            Color currentColor = IsMouseOver() ? hoverColor : backgroundColor;
+            Rectangle buttonRectangle = new Rectangle((int)position.X, (int)position.Y, 200, 50);
+            spriteBatch.Draw(GetTexture(currentColor), buttonRectangle, currentColor);
+
+            // Draw button text
+            Vector2 textSize = font.MeasureString(text);
+            Vector2 textPosition = position + new Vector2((buttonRectangle.Width - textSize.X) / 2, (buttonRectangle.Height - textSize.Y) / 2);
+            spriteBatch.DrawString(font, text, textPosition, Color.White);
+        }
+
+        public bool IsClicked(Point mousePosition)
+        {
+            Rectangle buttonRectangle = new Rectangle((int)position.X, (int)position.Y, 200, 50);
+            return buttonRectangle.Contains(mousePosition);
+        }
+
+        public bool IsMouseOver()
+        {
+            Rectangle buttonRectangle = new Rectangle((int)position.X, (int)position.Y, 200, 50);
+            return buttonRectangle.Contains(Mouse.GetState().Position);
+        }
+
+        private Texture2D GetTexture(Color color)
+        {
+            Texture2D texture = new Texture2D(graphicsDevice, 1, 1);
+            texture.SetData(new[] { color });
+            return texture;
+        }
+    }
+}
---
+++ FinalProject_KihoonKim_StefanKobetich/Entities/PlayerInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject_KihoonKim_StefanKobetich.Entities
+{
+    public static class PlayerInfo
+    {
+        public static string PlayerName { get; set; }
+        public static int PlayerScore { get; set; }
+        public static string GameMode { get; set; }
+
+        // 다른 초기화 로직이나 메서드가 필요한 경우 추가할 수 있습니다.
+
+        static PlayerInfo()
+        {
+            // 초기화 로직을 여기에 추가할 수 있습니다.
+            PlayerName = "";
+            PlayerScore = 0;
+            GameMode = "";
+        }
+    }
+
+}
